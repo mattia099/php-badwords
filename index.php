f54553c700488853af522d349cc535899302c31d
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-03-28 17:17:09 +0200

    paragrafo e lunghezza

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$paragrafo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure aperiam deleniti odio sed distinctio obcaecati eius dignissimos nulla?';
+$paragrago_lenght = strlen($paragrafo);
+echo $paragrafo;
+echo '<br>';
+echo $paragrago_lenght;
+
+
+?>
+
