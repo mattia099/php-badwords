6a93c28c1a23fcd489fc5e7ca1e53e625e163e85
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-03-28 17:24:34 +0200

    completo

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 $paragrafo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure aperiam deleniti odio sed distinctio obcaecati eius dignissimos nulla?';
 $paragrago_lenght = strlen($paragrafo);
+$censura = $_GET['censura'];
+$paragrafo_modificato = str_replace( $censura, '***', $paragrafo);
 echo $paragrafo;
 echo '<br>';
 echo $paragrago_lenght;
-
+echo '<br>';
+echo $paragrafo_modificato;
 
 ?>
 
